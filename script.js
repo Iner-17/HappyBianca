1f33cd9d95adc7f102ed3ac64a4766bd20df5fac
Iner-17 <inercollado52@gmail.com>
2024-02-11 13:10:20 +0800

    finished

---
+++ script.js
@@ -0,0 +1,62 @@
+const messageButton = document.querySelector('.open');
+let isOpen = false;
+let score = 0;
+
+const quiz = [
+    {
+        question: "What is your favorite color?",
+        answer: "blue"
+    },
+    {
+        question: "What's my pooping position?",
+        answer: "frog style"
+    },
+    {
+        question: "Sino mas mabait, bianca or bong",
+        answer: "bong"
+    },
+    {
+        question: "Sino mas mahaba pasensya, bianca or bong",
+        answer: "bong"
+    },
+    {
+        question: "Sino mas mabuti ang ugali, bianca or bong",
+        answer: "bong"
+    },
+    {
+        question: "Since you've reach this far I know it's you na hahaha and my last question iissssss WILL YOU BE MY VALENTINE? yes or no only. 😂😂😂",
+        answer: "yes"
+    }
+]
+
+
+const openMessage = () => {
+    if(!isOpen) {
+        alert("Hmmmm, I'm afraid you're not my BABY! You have to answer few questions first!")
+        for (let i = 0; i < quiz.length; i++) {
+            let userAnswer = prompt(quiz[i].question)
+            if (userAnswer.toLowerCase() == quiz[i].answer) {
+                score++;
+                alert("Correct")
+            } else {
+                alert("Eeeeenggkkk!")
+            }
+        }
+        if (score == 6) {
+            document.querySelector('.message').style.display = "block"
+        isOpen = true;
+        } else {
+            alert(`You only got ${score}!!!! TRY AGAINNNN`)
+            score = 0;
+        }
+        
+    } else {
+        document.querySelector('.message').style.display = "none"
+        isOpen = false;
+    }
+}
+
+
+
+
+messageButton.addEventListener('click', openMessage)
